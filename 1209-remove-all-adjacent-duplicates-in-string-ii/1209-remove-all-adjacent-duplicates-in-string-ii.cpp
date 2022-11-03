b7706a723d100c30aba4df61dba807d49a3392a5
nana7812 <54615046+nana7812@users.noreply.github.com>
2022-11-03 23:13:00 +0900

    Time: 80 ms (32.85%), Space: 10 MB (84.85%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {        
+        stack<pair<char,int>> st;
+        for(int i = 0; i < s.size(); i++){
+            if(st.empty() || st.top().first != s[i]){
+                st.push({s[i],1});
+            }else if(st.top().first == s[i]){
+                st.top().second++;
+            }
+            if(st.top().second == k){
+                st.pop();
+            }
+        }
+        
+        string ans = "";
+        while(!st.empty()){
+            while(st.top().second--){
+                ans += st.top().first;
+            }
+            st.pop();
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
