2871666c3b5919a136181c85c3e2bbd7260243ae
nana7812 <54615046+nana7812@users.noreply.github.com>
2022-11-05 21:54:04 +0900

    Time: 4 ms (87.52%), Space: 9.3 MB (53.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+    
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string s1 = strs[0];
+        string s2 = strs[strs.size()-1]; 
+        
+        string ans;
+        for(int i = 0; i < s1.size(); i++){
+            if(s1[i] == s2[i]){
+                ans += s1[i];
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+
+};
