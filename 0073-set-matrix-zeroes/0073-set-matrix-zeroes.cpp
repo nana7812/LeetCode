9cccd31a55275268ebc245483821b4c9f7031efd
nana7812 <54615046+nana7812@users.noreply.github.com>
2022-11-24 23:00:56 +0900

    Time: 25 ms (67.71%), Space: 13.3 MB (20.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,-1,1};
-    
-    int N,M;
-    queue<pair<pair<int,int>,int>> q;
+  
     void setZeroes(vector<vector<int>>& matrix) {
-        N = matrix.size();
-        M = matrix[0].size();
-        bool c[200][200][4] = {false,};
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(matrix[i][j] == 0){
-                    for(int d = 0; d< 4; d++){
-                        q.push({{i,j},d});
-                        c[i][j][d] = true;
-                    }
-                }
-            }
-        }
-        
-        while(!q.empty()){
-            int qx = q.front().first.first;
-            int qy = q.front().first.second;
-            int qd = q.front().second;
-            q.pop();
-            while(0<=qx + dx[qd] && qx + dx[qd] < N && 0<= qy + dy[qd] && qy + dy[qd] < M && c[qx + dx[qd]][qy + dy[qd]][qd] == false){
-                qx += dx[qd];
-                qy += dy[qd];
-                c[qx][qy][qd] = true;
-                matrix[qx][qy] = 0;
-            }
-        }
+    vector<int> rows(matrix.size(),1), cols(matrix[0].size(),1);
+           for( int i = 0; i < matrix.size(); i++){                 
+             for(int j = 0; j< matrix[0].size(); j++){
+	             if(matrix[i][j] == 0){
+		            rows[i] = cols[j] = 0;       //updating hash tables
+			     }
+	         }
+           }
+//Now updating the matrix in 2nd traversal
+      for( int i = 0; i < matrix.size(); i++){                 
+          for(int j = 0; j< matrix[0].size(); j++){
+	          if(rows[i] == 0 || cols[j] == 0){
+		         matrix[i][j] = 0;       
+			 }
+	   }
+      }
     }
+    
+
+    
+    
+    
 };
