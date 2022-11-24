6ed6048f1b896db160fafcf8d933c0a1104c277f
nana7812 <54615046+nana7812@users.noreply.github.com>
2022-11-24 23:02:48 +0900

    Time: 29 ms (57.25%), Space: 13.3 MB (20.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-  
     void setZeroes(vector<vector<int>>& matrix) {
-    vector<int> rows(matrix.size(),1), cols(matrix[0].size(),1);
-           for( int i = 0; i < matrix.size(); i++){                 
-             for(int j = 0; j< matrix[0].size(); j++){
-	             if(matrix[i][j] == 0){
-		            rows[i] = cols[j] = 0;       //updating hash tables
-			     }
-	         }
-           }
-//Now updating the matrix in 2nd traversal
-      for( int i = 0; i < matrix.size(); i++){                 
-          for(int j = 0; j< matrix[0].size(); j++){
-	          if(rows[i] == 0 || cols[j] == 0){
-		         matrix[i][j] = 0;       
-			 }
-	   }
-      }
-    }
-    
-
-    
-    
-    
+        vector<int> rows(matrix.size(),1), cols(matrix[0].size(),1);
+        for(int i = 0; i < matrix.size(); i++){                 
+            for(int j = 0; j< matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = cols[j] = 0;       //updating hash tables
+			    }
+	        }
+        }
+        
+      //Now updating the matrix in 2nd traversal
+        for( int i = 0; i < matrix.size(); i++){                 
+            for(int j = 0; j< matrix[0].size(); j++){
+                if(rows[i] == 0 || cols[j] == 0){
+                    matrix[i][j] = 0;       
+                }
+            }
+        }
+    } 
 };
