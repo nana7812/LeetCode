babd64469bd753a966c6708fca9bae03d4d99ff7
nana7812 <54615046+nana7812@users.noreply.github.com>
2023-01-03 23:05:54 +0900

    Time: 5 ms (21.32%), Space: 6.8 MB (56.28%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        if(n % 2 != 0) {
+            ans.push_back(0);
+        }
+        
+        for(int i = 1; i <= n/2; i++){
+            ans.push_back(i);
+            ans.push_back((-1)*i);
+        }
+        
+        sort(ans.begin(),ans.end());
+    
+        return ans;
+    }
+};
+
